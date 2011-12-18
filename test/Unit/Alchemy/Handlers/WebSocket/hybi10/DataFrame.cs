dbd5576b14d74b2f4259bcdfe704cf9af581f014
Drew Ditthardt <dditthardt@olivinelabs.com>
2011-12-18 01:18:46 -0800

    Made Tests a little better for dataframe on hybi10

--- test/Unit/Alchemy/Handlers/WebSocket/hybi10/DataFrame.cs
+++ test/Unit/Alchemy/Handlers/WebSocket/hybi10/DataFrame.cs
@@ -25,39 +25,60 @@ namespace Alchemy.Handlers.WebSocket.hybi10
         [Test]
         public void BackAndForthSmall()
         {
-            byte[] testData = new byte[50];
+            byte[] testData = GenerateRandomArray(50);
             _dataframe.Append(testData);
-
-            _dataframe.AsFrame();
-
-            List<ArraySegment<byte>> list = _dataframe.AsRaw();
-
-            var temp = new byte[0];
-            foreach(var someData in list)
-            {
-                int len = temp.Length;
-                var temp2 = new byte[someData.Array.Length + len];
-                Array.Copy(someData.Array, 0, temp2, len, someData.Array.Length - len);
-                Array.Copy(temp, 0, temp2, 0, len);
-                temp = temp2;
-            }
-            Assert.AreEqual(temp.Length, testData.Length);
-            for(int index = 0; index < temp.Length; index++)
-            {
-                Assert.AreEqual(testData[index], temp[index]);
-            }
+            TestBackAndForth();
         }
 
         [Test]
         public void BackAndForthMedium()
         {
-
+            byte[] testData = GenerateRandomArray(10000);
+            _dataframe.Append(testData);
+            TestBackAndForth();
         }
 
         [Test]
         public void BackAndForthBig()
         {
+            int dataSize = 1000000000;
+            while (dataSize > 0)
+            {
+                byte[] testData = GenerateRandomArray(512);
+                _dataframe.Append(testData);
+                dataSize -= 512;
+            }
 
+            TestBackAndForth();
+        }
+
+        public void TestBackAndForth()
+        {
+            List<ArraySegment<byte>> tempList = _dataframe.AsRaw();
+            List<ArraySegment<byte>> originalList = tempList.Select(item => new ArraySegment<byte>((byte[]) item.Array.Clone())).ToList();
+            _dataframe.AsFrame();
+
+            List<ArraySegment<byte>> list = _dataframe.AsRaw();
+
+            Assert.AreEqual(list.Count, originalList.Count);
+            for (int index = 0; index < list.Count; index++)
+            {
+                for (int index2 = 0; index2 < list[index].Array.Length; index2++)
+                {
+                    Assert.AreEqual(((ArraySegment<byte>)(originalList[index])).Array[index2], list[index].Array[index2]);
+                }
+            }
+        }
+
+        public static byte[] GenerateRandomArray(int nbOfRows)
+        {
+              const int minNumber = 0x0;
+              const int maxNumber = 0xff;
+              Random randomNumber = new Random();
+              byte[] newarray = new byte[nbOfRows];
+              for (int row = 0; row < nbOfRows; row++)
+                newarray[row] = (byte)randomNumber.Next(minNumber,maxNumber);
+              return newarray;
         }
     }
 }
